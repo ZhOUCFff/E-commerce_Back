a45d92a86109db2d48b8626e63bca4716f1e6a25
ZhOUCFff <1757775936@qq.com>
2020-04-15 09:31:26 +0800

    测试接口

--- web/src/network/request.js
+++ web/src/network/request.js
@@ -4,8 +4,8 @@ import NProgress from 'nprogress'
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8888/api/private/v1',
-    // baseURL: process.env.NODE_ENV === "production"?'/api/private/v1':'http://127.0.0.1:8888/api/private/v1',
+    // baseURL: 'http://127.0.0.1:8888/api/private/v1',
+    baseURL: process.env.NODE_ENV === "production"?'/api/private/v1':'http://localhost:8888/api/private/v1',
     timeout: 5000
   })
   // 拦截请求
